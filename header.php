bf0cd4cb4dfa6e8f90febcaffa29097e6b848214
Yari Andres <yari.andres@gmail.com>
2019-08-22 17:33:33 +0200

    adding pages file

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="This is a meta description. this will be used for search engines">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP loggin System</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+  <header>
+
+    <nav>
+      <a href="#">Loging System</a>
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Porfolio</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+    </nav>
+ 
+  </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  require "header.php";
+?>
+
+  <main>
+    
+     <h1>The main content of the site!</h1>
+ 
+  </main>
+
+<?php
+  require "footer.php";
+?>
