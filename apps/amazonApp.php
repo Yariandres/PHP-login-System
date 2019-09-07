55baa4e1f2785c1db88e4005fea4e5c0eb6820b7
Yari Andres <yari.andres@gmail.com>
2019-09-07 13:41:52 +0200

    html fixtures

--- apps/amazonApp.php
+++ apps/amazonApp.php
@@ -1,4 +1,65 @@
 
-<section class="amz-main-app">
-  <h1 class="mt-5"> BOOM! - Lets get started!</h1>;
+<section class="main-app container">
+  <div class="jumbotron">
+    <h1 class="mt-5 text-center"> BOOM! - Lets get started!</h1>;
+  </div>
+
+  <form method="post">
+
+  <!-- ABOUT THE PRODUCT -->
+  <h2 class="my-4 lead">About The Product</h2>
+
+    <div class="form-group row">
+      <label for="q1" class="col-sm-6 col-form-label">Name of the product: (Include the name brand last) 1. What is the name of the product your
+        are selling. 2. Start with the main keyword. (Ex: Kitchen Wooden Spoon 6 set - Cotton Flight Pillow - Iphone Cover)</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="q1" id="q1" placeholder="Ex: Iphone Cover By Icare LTD">
+      </div>
+    </div>
+
+    <div class="form-group row mb-4">
+      <label for="q2" class="col-sm-6 col-form-label">Type of product: What exactly are you selling.<br> (ex: Wooden Spoons - Flight Pillow - Phone
+        Cover)</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="q2" id="q2" placeholder="Ex: Phone Cover">
+      </div>
+    </div>
+    <hr>
+    <!-- TARGET AUDIENCE -->
+    <h2 class="my-4 lead">Target Audience</h2>
+
+    <div class="form-group row">
+      <label for="q3" class="col-sm-6 col-form-label">Target Audience for this product: (Singular Word)<br> (ex: A Chef - Executive - Iphone Owner)</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="q3" id="q3" placeholder="Ex: Iphone Owner">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="q4" class="col-sm-6 col-form-label">Target Audience for this product: (Plural Word) "........ are the target audience" (ex:
+        Chefs - Executives - Iphone Owners)</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="q4" id="q4" placeholder="Ex: Iphone Owners">
+      </div>
+    </div>
+
+    <hr>
+    <h2 class="my-4 lead">Keyword Theme</h2>
+
+    <div class="form-group row">
+      <label for="q5" class="col-sm-6 col-form-label">In 2 to 3 words: What are the main area of focus for this product (ex: Cook faster - Helps you relax - Save from smashing)</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="q5" id="q5" placeholder="Ex: Save from smashing...">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="q6" class="col-sm-6 col-form-label">How does this product helps, your target audience? "This product helps you with ........ Verb ending in "ing")" (ex: Mixing food perfectly everytime - Relaxing in long journeys - Protecting your phone)</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="q6" id="q6" placeholder="Ex: Protecting your phone...">
+      </div>
+    </div>
+    
+  </form>
+  
 </section>
